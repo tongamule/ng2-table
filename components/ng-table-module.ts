b59cb1f822b0236bed013088ed4ed7e4f6a4d443
Gaston Mulé <gmule@midasconsultores.com.ar>
2017-09-12 15:28:58 -0300

    actualizo dependencias

--- components/ng-table-module.ts
+++ components/ng-table-module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DropdownModule } from 'ng2-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { NgTableComponent } from './table/ng-table.component';
 import { NgTableFilteringDirective } from './table/ng-table-filtering.directive';
@@ -9,7 +9,7 @@ import { NgTablePagingDirective } from './table/ng-table-paging.directive';
 import { NgTableSortingDirective } from './table/ng-table-sorting.directive';
 
 @NgModule({
-  imports: [CommonModule, DropdownModule],
+  imports: [CommonModule, BsDropdownModule],
   declarations: [NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective],
   exports: [NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective]
 })
